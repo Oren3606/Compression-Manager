c40c6ab5cd122c0fbb87422c92d5ff646b2605e3
Oren3606 <oren.dev@icloud.com>
2023-11-24 18:09:51 +0200

    updated ignored files- 'venv' and '.idea'

---
+++ uncompress.py
@@ -0,0 +1,42 @@
+def uc_zip(bytes):
+    pass
+
+
+def uc_rar(bytes):
+    pass
+
+
+def uc_iso(bytes):
+    pass
+
+
+def uc_tar(bytes):
+    pass
+
+
+def uc_gz(bytes):
+    pass
+
+
+def uc_7z(bytes):
+    pass
+
+
+def uc_apk(bytes):
+    pass
+
+
+def uc_dmg(bytes):
+    pass
+
+
+def uc_jar(bytes):
+    pass
+
+
+def uc_cab(bytes):
+    pass
+
+
+def uc_exe(bytes):
+    pass
