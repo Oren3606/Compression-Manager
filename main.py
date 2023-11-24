68aa10aeda54a5fac4863535399dce59a8f231f0
Oren3606 <oren.dev@icloud.com>
2023-11-24 20:36:45 +0200

    updated ignored files- 'venv' and '.idea'

--- main.py
+++ main.py
@@ -5,15 +5,22 @@ api
 # imports
 
 # todo: handle launch either from CLI or from GUI
+mode = bool #compress or uncompress
+directory = str # dir location
+type = str # file type
 
-directory = int
-type = str
-file_bytes = int
-
-with open('README.md', 'rb') as p_file:
+with open('elevator.exe', 'rb') as p_file:
     content = p_file.read()
-    print("erngierrgu" + type(content))
-    to_bytes
+    print(type(content[1]))
+  #  bin = ''
+  #  print(content)
+  #  for ch in content:
+ #       while ch>8:
+  #          bin+='1'
+  #          ch-=8
+  #      bin += ord(ch)
+
+    print(bin)
     # while (content):  # if there are still bytes to iterate through
     #   cur_byte = content[0:8]
     #  for i in range(0, 8):
