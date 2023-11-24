ce6f9a8e542adccdc749106d46956b91b0fada13
Oren3606 <oren.dev@icloud.com>
2023-11-25 01:11:04 +0200

    updates to names and experimenting

---
+++ decode.py
@@ -0,0 +1,42 @@
+def uc_zip(file):
+    pass
+
+
+def uc_rar(file):
+    pass
+
+
+def uc_iso(file):
+    pass
+
+
+def uc_tar(file):
+    pass
+
+
+def uc_gz(file):
+    pass
+
+
+def uc_7z(file):
+    pass
+
+
+def uc_apk(file):
+    pass
+
+
+def uc_dmg(file):
+    pass
+
+
+def uc_jar(file):
+    pass
+
+
+def uc_cab(file):
+    pass
+
+
+def uc_exe(file):
+    pass
