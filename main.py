53ef110468ac4d7bdc1baae122dd33db1e6e20f6
Oren3606 <bohako111@gmail.com>
2023-11-24 19:10:33 +0200

    updated ignored files- 'venv' and '.idea'

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 """
-
+api
 """
 
 # imports
+
+# todo: handle launch either from CLI or from GUI
+
+directory = int
+type = str
+file_bytes = int
+
+with open('README.md', 'rb') as p_file:
+    content = p_file.read()
+    print("erngierrgu" + type(content))
+    to_bytes
+    # while (content):  # if there are still bytes to iterate through
+    #   cur_byte = content[0:8]
+    #  for i in range(0, 8):
+    #     pass
