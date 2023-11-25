94efa0f3ee0c6fe680c497f42155b5fcb65f5a5b
Oren3606 <oren.dev@icloud.com>
2023-11-26 00:44:05 +0200

    slight changes

--- main.py
+++ main.py
@@ -3,15 +3,29 @@ api
 """
 
 # imports
+from os import path, scandir
 
 # todo: handle launch either from CLI or from GUI
-mode = bool  # compress or uncompress
-directory = str  # dir location
-extension = str  # file type
+mode = bool  # compress or uncompress: True for compression, False for decompression
+directory = scandir(None)  # dir location, todo: get from launch
+
+
+if mode:
+    from os import scandir
+
+else:
+    pass
+
+
+s = scandir('experimentation- file types')
+print(s)
+# if not mode:
+for file in s:
+    print(file.name)
 
 wholefile = open('experimentation- file types/exe.exe', 'rb').read()
 st = ''
 
-for byte in wholefile:
-    st += str(byte)
-    print(byte, end=' ')
+# for byte in wholefile:
+#   st += str(byte)
+#  print(byte, end=' ')
