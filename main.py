2e037a69568f297d948386b8339bdf57fffa4443
Oren3606 <oren.dev@icloud.com>
2023-12-21 17:05:34 +0200

    added settings.json for compression configuration

--- main.py
+++ main.py
@@ -7,16 +7,16 @@ from os import path, scandir
 from json import load
 
 # todo: handle launch either from CLI or from GUI
+# with open("settings.json") as settings_file:
+#    settings = load(settings_file)
 mode = bool  # compress or uncompress: True for compression, False for decompression
 directory = scandir(None)  # dir location, todo: get from launch
-
 
 if mode:
     from os import scandir
 
 else:
     pass
-
 
 s = scandir('experimentation- file types')
 print(s)
