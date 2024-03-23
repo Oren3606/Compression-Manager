a53d30db4adf1a04c15dee293bb2bac7f8784766
Oren3606 <bohako111@gmail.com>
2024-03-23 18:33:32 +0200

    sync

--- main.py
+++ main.py
@@ -14,20 +14,6 @@ mode = bool  # compress or uncompress: True for compression, False for decompres
 
 # todo: check all settings exist, else add defaults
 # todo instead of this if block, import decode OR encode which will have corresponding imports?
-if mode:
-    from os import scandir
-
-
-else:
-    from tempfile import gettempdir as tmp_dir
-
-s = scandir('experimentation- file types')
-print(s)
-for file in s:
-    print(file.name)
-
-wholefile = open('experimentation- file types/exe.exe', 'rb').read()
-st = ''
 
 # for byte in wholefile:
 #   st += str(byte)
